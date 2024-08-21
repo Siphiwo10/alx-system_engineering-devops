66ae881459e87c95265f9d9dfc0eec42d5a87f7b
root <siphiwoj10@gmail.com>
2024-08-21 11:33:00 +0000

    Regular Expressions

--- 0x06-regular_expressions/100-textme.rb
+++ 0x06-regular_expressions/100-textme.rb
@@ -1,3 +1,6 @@
 #!/usr/bin/env ruby
-# A regular expression that is matches 10 digit phone number
-puts ARGV[0].scan(/^[0-9]{10}$/).join
+
+from = ARGV[0].scan(/from:(.*?)\]/)
+to = ARGV[0].scan(/to:(.*?)\]/)
+flags = ARGV[0].scan(/flags:(.*?)\]/)
+puts [from, to, flags].join(',')
--- 0x06-regular_expressions/7-OMG_WHY_ARE_YOU_SHOUTING.rb
+++ 0x06-regular_expressions/7-OMG_WHY_ARE_YOU_SHOUTING.rb
@@ -1,3 +1,3 @@
 #!/usr/bin/env ruby
-# A regular expression that is matches 10 digit phone number
-puts ARGV[0].scan(/^[0-9]{10}$/).join
+# A regular expression that is matches only capital letters
+puts ARGV[0].scan(/[A-Z]/).join
